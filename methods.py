b9be31e2822c465b7789aa214457ed0ea5e6e7c5
mrifkikurniawan <mrifkikurniawan17@gmail.com>
2021-08-12 20:17:16 +0700

    import ewc

--- methods.py
+++ methods.py
@@ -1,6 +1,7 @@
 from easydict import EasyDict as edict
 
 import torch.nn as nn
+from avalanche.training.plugins import EWCPlugin
 
 from class_strategy import *
 from utils import create_instance
@@ -37,3 +38,36 @@ class NaiveFinetune(object):
     
     def initialize_plugins(self):
         return ClassStrategyPlugin()
+    
+    
+class EWC(object):
+    def __init__(self, 
+                 model: edict,
+                 optimizer: edict,
+                 criterion: edict
+                 ):
+        
+        self._model = create_instance(model)
+        self._model.fc = MLP(2048, 7)
+        self._optimizer = create_instance(optimizer, params=self._model.parameters())
+        self._criterion = create_instance(criterion) 
+        self._plugins = self.initialize_plugins()
+    
+    @property
+    def model(self):
+        return self._model
+    
+    @property
+    def optimizer(self):
+        return self._optimizer
+    
+    @property
+    def criterion(self):
+        return self._criterion
+    
+    @property
+    def plugins(self):
+        return self._plugins
+    
+    def initialize_plugins(self):
+        return EWCPlugin(ewc_lambda=0.4, decay_factor=0.1)
