4da0c79c6adf4fb9a58bc0434a6f8208c836d65c
mrifkikurniawan <mrifkikurniawan17@gmail.com>
2021-08-18 22:11:13 +0700

    train closes2center replay

--- train.sh
+++ train.sh
@@ -1,8 +1,8 @@
 python3.9 classification.py \
---name logs/replay/replay_class_balanced_herding \
+--name logs/replay/replay_class_balanced_closest2center \
 --root /home/rifki/continual_learning/datasets \
 --num_workers 16 \
 --store \
 --gpu_id 4 \
---config configs/replay/replay_class_balanced_herding.yaml \
---comment replay_class_balanced_herding
+--config configs/replay/replay_class_balanced_closest2center.yaml \
+--comment replay_class_balanced_closest2center
