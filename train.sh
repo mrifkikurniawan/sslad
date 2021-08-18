df6356d1e517326d1817a3d35e6bd776c2f8771b
mrifkikurniawan <mrifkikurniawan17@gmail.com>
2021-08-18 20:26:06 +0700

    remove nohup

--- train.sh
+++ train.sh
@@ -1,9 +1,8 @@
-nohup python3.9 classification.py \
+python3.9 classification.py \
 --name logs/replay/replay_class_balanced_random \
 --root /home/rifki/continual_learning/datasets \
 --num_workers 16 \
 --store \
 --gpu_id 4 \
 --config configs/replay/replay_class_balanced_random.yaml \
---comment replay_class_balanced_random \
-> nohup/replay.out &
+--comment replay_class_balanced_random
