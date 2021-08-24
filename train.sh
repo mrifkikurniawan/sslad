6c9b517b90105a17ee90744e68b402a91d37c0dd
mrifkikurniawan <mrifkikurniawan17@gmail.com>
2021-08-25 05:37:49 +0700

    train si

--- train.sh
+++ train.sh
@@ -1,9 +1,9 @@
 python3.9 classification.py \
---name logs/replay/replay_experienced_balance_random_bs8 \
+--name logs/si/si_0.4 \
 --root /home/rifki/continual_learning/datasets \
 --num_workers 32 \
 --store \
 --gpu_id 4 \
---config configs/replay/replay_experienced_balance_random.yaml \
---comment replay_experienced_balance_random_bs8 \
+--config configs/si/si_0.4.yaml \
+--comment si_0.4 \
 --store_model
