9cd84b29fdb68443f57b31df519e1f6fc34a17ea
mrifkikurniawan <mrifkikurniawan17@gmail.com>
2021-08-24 23:16:36 +0700

    change bs to 8

--- train.sh
+++ train.sh
@@ -1,9 +1,9 @@
 python3.9 classification.py \
---name logs/replay/replay_experienced_balance_random \
+--name logs/replay/replay_experienced_balance_random_bs8 \
 --root /home/rifki/continual_learning/datasets \
 --num_workers 32 \
 --store \
 --gpu_id 4 \
 --config configs/replay/replay_experienced_balance_random.yaml \
---comment replay_experienced_balance_random
+--comment replay_experienced_balance_random_bs8 \
 --store_model
