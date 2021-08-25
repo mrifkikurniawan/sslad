910b13d4156dcb44b849a34abbd74e4616afcc4b
mrifkikurniawan <mrifkikurniawan17@gmail.com>
2021-08-25 10:46:35 +0700

    add agem sample size 32

--- train.sh
+++ train.sh
@@ -1,9 +1,9 @@
 python3.9 classification.py \
---name logs/agem/agem_10 \
+--name logs/agem/agem_32 \
 --root /home/rifki/continual_learning/datasets \
 --num_workers 32 \
 --store \
 --gpu_id 4 \
---config configs/agem/agem_10.yaml \
---comment agem_10 \
+--config configs/agem/agem_32.yaml \
+--comment agem_32 \
 --store_model
