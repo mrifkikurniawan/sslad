23f5bacb5a4e2290f83b9df4a80a324a3000b2fb
mrifkikurniawan <mrifkikurniawan17@gmail.com>
2021-08-25 10:36:18 +0700

    set agem sample to 10

--- train.sh
+++ train.sh
@@ -1,9 +1,9 @@
 python3.9 classification.py \
---name logs/agem/agem_16 \
+--name logs/agem/agem_10 \
 --root /home/rifki/continual_learning/datasets \
 --num_workers 32 \
 --store \
 --gpu_id 4 \
---config configs/agem/agem_16.yaml \
---comment agem_16 \
+--config configs/agem/agem_10.yaml \
+--comment agem_10 \
 --store_model
