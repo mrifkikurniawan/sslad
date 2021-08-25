d03b9aab735d4ef091d2d87922b4170dbad9942a
mrifkikurniawan <mrifkikurniawan17@gmail.com>
2021-08-25 07:54:03 +0700

    set weight to 0.6

--- train.sh
+++ train.sh
@@ -1,9 +1,9 @@
 python3.9 classification.py \
---name logs/si/si_0.4 \
+--name logs/si/si_0.6 \
 --root /home/rifki/continual_learning/datasets \
 --num_workers 32 \
 --store \
 --gpu_id 4 \
 --config configs/si/si_0.4.yaml \
---comment si_0.4 \
+--comment si_0.6 \
 --store_model
