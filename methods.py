73148e046c2de8bc2d45185b1f2d967e494f8ad0
mrifkikurniawan <mrifkikurniawan17@gmail.com>
2021-08-25 11:13:12 +0700

    add cope strategy

--- methods.py
+++ methods.py
@@ -1,7 +1,7 @@
 from easydict import EasyDict as edict
 
 import torch.nn as nn
-from avalanche.training.plugins import EWCPlugin, ReplayPlugin, SynapticIntelligencePlugin, AGEMPlugin, LwFPlugin
+from avalanche.training.plugins import EWCPlugin, ReplayPlugin, SynapticIntelligencePlugin, AGEMPlugin, LwFPlugin, CoPEPlugin
 
 from class_strategy import *
 from utils import create_instance
@@ -206,6 +206,8 @@ class AGEM(object):
     def initialize_plugins(self):
         return AGEMPlugin(**self._kwargs)
     
+    
+    
 class LwF(object):
     def __init__(self, 
                  model: edict,
@@ -237,3 +239,39 @@ class LwF(object):
     
     def initialize_plugins(self):
         return LwFPlugin()
+    
+    
+    
+class CoPE(object):
+    def __init__(self, 
+                 model: edict,
+                 optimizer: edict,
+                 criterion: edict,
+                 **kwargs
+                 ):
+        
+        self._model = create_instance(model)
+        self._model.fc = nn.Linear(2048, 7, bias=False)
+        self._optimizer = create_instance(optimizer, params=self._model.parameters())
+        self._criterion = create_instance(criterion) 
+        self._kwargs = kwargs
+        self._plugins = self.initialize_plugins()
+    
+    @property
+    def model(self):
+        return self._model
+    
+    @property
+    def optimizer(self):
+        return self._optimizer
+    
+    @property
+    def criterion(self):
+        return self._criterion
+    
+    @property
+    def plugins(self):
+        return self._plugins
+    
+    def initialize_plugins(self):
+        return CoPEPlugin(**self._kwargs)
--- train.sh
+++ train.sh
@@ -1,9 +1,9 @@
 python3.9 classification.py \
---name logs/lwf/lwf \
+--name logs/cope/cope \
 --root /home/rifki/continual_learning/datasets \
 --num_workers 32 \
 --store \
 --gpu_id 4 \
---config configs/lwf.yaml \
---comment lwf \
+--config configs/cope/cope.yaml \
+--comment cope \
 --store_model
