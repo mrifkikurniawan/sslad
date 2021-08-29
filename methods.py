fba76f508c920cd9f815b9db71efee0967d860c9
mrifkikurniawan <mrifkikurniawan17@gmail.com>
2021-08-29 19:21:08 +0700

    add clstrategy

--- methods.py
+++ methods.py
@@ -7,6 +7,36 @@ from class_strategy import *
 from utils import create_instance
 from models import MLP
 
+class CLStrategy(object):
+    def __init__(self, 
+                 model: edict,
+                 optimizer: edict,
+                 criterion: edict,
+                 plugins: edict
+                 ):
+        
+        self._model = create_instance(model)
+        self._model.fc = nn.Linear(2048, 7, bias=False)
+        self._optimizer = create_instance(optimizer, params=self._model.parameters())
+        self._criterion = create_instance(criterion) 
+        self._plugins = create_instance(plugins)
+    
+    @property
+    def model(self):
+        return self._model
+    
+    @property
+    def optimizer(self):
+        return self._optimizer
+    
+    @property
+    def criterion(self):
+        return self._criterion
+    
+    @property
+    def plugins(self):
+        return self._plugins
+    
 class NaiveFinetune(object):
     def __init__(self, 
                  model: edict,
