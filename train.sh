0d9c173347cf51ca85e43963274c212ab5f40b7b
mrifkikurniawan <mrifkikurniawan17@gmail.com>
2021-09-03 13:21:27 +0800

    cl strategy rm, gpu id

--- train.sh
+++ train.sh
@@ -1,9 +1,9 @@
 python3.9 classification.py \
---name logs/cl_strategy/cl_strategy_aug \
+--name logs/cl_strategy/cl_strategy_rm \
 --root /home/rifki/continual_learning/datasets \
 --num_workers 32 \
 --store \
---gpu_id 4 \
+--gpu_id 0 \
 --config configs/cl_strategy.yaml \
---comment cl_strategy \
+--comment cl_strategy_rm \
 --store_model
