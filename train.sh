ae439193c0966651d2b33f0f71c9bf6e80a0992d
mrifkikurniawan <mrifkikurniawan17@gmail.com>
2021-09-17 23:02:10 +0800

    imbalanced train

--- train.sh
+++ train.sh
@@ -1,9 +1,9 @@
 python3.9 classification.py \
---name logs/cl_strategy/cl_strategy_uncertainty_entropy \
+--name logs/cl_strategy/cl_strategy_online_imbalanced_sampler \
 --root /home/rifki/continual_learning/datasets \
 --num_workers 8 \
 --store \
 --gpu_id 4 \
 --config configs/cl_strategy.yaml \
---comment cl_strategy_uncertainty_entropy \
+--comment cl_strategy_online_imbalanced_sampler \
 --store_model
