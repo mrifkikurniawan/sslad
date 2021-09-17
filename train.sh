ad0f5c6e95634b8041b2a67a11c3688b939ce7ac
mrifkikurniawan <mrifkikurniawan17@gmail.com>
2021-09-17 19:15:31 +0800

    modif train sh

--- train.sh
+++ train.sh
@@ -1,9 +1,9 @@
 python3.9 classification.py \
 --name logs/cl_strategy/cl_strategy_uncertainty_entropy \
 --root /home/rifki/continual_learning/datasets \
---num_workers 32 \
+--num_workers 8 \
 --store \
---gpu_id 0 \
+--gpu_id 4 \
 --config configs/cl_strategy.yaml \
 --comment cl_strategy_uncertainty_entropy \
 --store_model
