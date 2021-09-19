7157eaa73c74e5505c724f59dd825675c96fba4f
mrifkikurniawan <mrifkikurniawan17@gmail.com>
2021-09-19 23:07:12 +0700

    add softmax with temperature module

--- modules.py
+++ modules.py
@@ -313,3 +313,22 @@ class Prototypes(object):
         return self._embedding
     
     
+    
+class SoftmaxT(nn.Module):
+
+    def __init__(self, temperature: Union[float, int, torch.Tensor]) -> None:
+        super(SoftmaxT, self).__init__()
+
+        if isinstance(temperature, float) or isinstance(temperature, int):
+            temperature = torch.tensor([temperature])
+        self.temperature = temperature
+
+    def forward(self, inputs: torch.Tensor, act_f: str) -> torch.Tensor:
+        temperature = self.temperature.type_as(inputs)
+        out = inputs/temperature
+
+        if act_f == 'softmax':
+            out = F.softmax(out, dim=1)
+        elif act_f == 'log_softmax':
+            out = F.log_softmax(out, dim=1)
+        return out    
