6c05051377265831d16051b3373fa4c92ea7f468
mrifkikurniawan <mrifkikurniawan17@gmail.com>
2021-09-24 20:05:35 +0700

    add metric learning wrapper

--- modules.py
+++ modules.py
@@ -448,3 +448,23 @@ class ForwardTransfer(object):
             loss = torch.tensor(0.0).to(next(model.parameters()).device)
         
         return loss
+    
+    
+    
+class MetricLearner():
+    def __init__(self, losses: List[dict]):
+        
+        # create loss instances
+        self.loss_modules: List[dict] = list()
+        for loss in losses:
+            loss = edict(loss)
+            loss_module = edict(module=create_instance(loss),
+                                weight=loss.weight)
+            self.loss_modules.append(loss_module)
+            
+    def __call__(self, embeddings: torch.Tensor, labels: torch.Tensor):
+        loss = torch.tensor(0.0).type_as(embeddings)
+        for i in range(len(self.loss_modules)):
+            loss += self.loss_modules[i].module(embeddings, labels) * (self.loss_modules[i].weight).type_as(loss)
+        
+        return loss
