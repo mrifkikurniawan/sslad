ba506615e951cef3724954e725ba671953176882
mrifkikurniawan <mrifkikurniawan17@gmail.com>
2021-09-25 11:39:19 +0700

    initial commit hparams search

---
+++ train_hparams_search.sh
@@ -0,0 +1,8 @@
+python3.9 classification_optuna.py \
+--name logs/cl_strategy/cl_strategy_online_imbalanced_sampler \
+--root /home/rifki/continual_learning/datasets \
+--num_workers 8 \
+--store \
+--gpu_id 4 \
+--config configs/cl_strategy.yaml \
+--comment cl_strategy_online_imbalanced_sampler 
