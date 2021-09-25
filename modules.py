c4ae63b3ae82c9e843f896e48ac27b258a13dd15
mrifkikurniawan <mrifkikurniawan17@gmail.com>
2021-09-25 09:25:33 +0800

    add object major, convert weight tensor

--- modules.py
+++ modules.py
@@ -451,7 +451,7 @@ class ForwardTransfer(object):
     
     
     
-class MetricLearner():
+class MetricLearner(object):
     def __init__(self, losses: List[dict]):
         
         # create loss instances
@@ -459,12 +459,11 @@ class MetricLearner():
         for loss in losses:
             loss = edict(loss)
             loss_module = edict(module=create_instance(loss),
-                                weight=loss.weight)
+                                weight=torch.tensor(loss.weight))
             self.loss_modules.append(loss_module)
             
     def __call__(self, embeddings: torch.Tensor, labels: torch.Tensor):
         loss = torch.tensor(0.0).type_as(embeddings)
         for i in range(len(self.loss_modules)):
             loss += self.loss_modules[i].module(embeddings, labels) * (self.loss_modules[i].weight).type_as(loss)
-        
         return loss
