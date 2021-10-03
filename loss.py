1eefcfaf2c07c776ffaac220deeeca49c12acc9e
mrifkikurniawan <mrifkikurniawan17@gmail.com>
2021-10-03 14:35:20 +0800

    add class balanced loss

--- loss.py
+++ loss.py
@@ -149,8 +149,28 @@ class CostSenstiveLoss(nn.Module):
     
 
 class ClassBalancedLoss(nn.Module): 
-    def __init__(self):
-        pass
+    def __init__(self, 
+                 loss: dict=None,
+                 beta: float=0.9,
+                 num_classes: int=7, 
+                 **kwargs):
+        super().__init__()
+        
+        self.beta = torch.tensor(beta) 
+        self.loss = create_instance(loss)
+        self.num_classes = num_classes
+        self.kwargs = kwargs
+        self.dataset_distribution = torch.zeros(self.num_classes)
+        self.num_iter = 0
+        self.epsilon = 1e-6
     
-    def forward(self):
-        pass
+    def forward(self, inputs: torch.Tensor, targets: torch.Tensor):
+        loss = torch.tensor(0.0).type_as(inputs)
+        distribution = get_batch_distribution(targets, self.num_classes) + self.epsilon
+        weight = ((1.0 - self.beta) / (1.0 - self.beta**distribution)).type_as(inputs)
+        loss += self.loss(inputs, targets, weight=weight, **self.kwargs)
+        
+        self.num_iter += 1
+        self.dataset_distribution += distribution
+        
+        return loss
